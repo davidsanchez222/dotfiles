471a854208205570fc78224ee988a7f2d68e2181
davidsanchez222 <david100sanchez2003@gmail.com>
2025-08-27 17:39:14 -0400

    edits: added karabiner, small changes to ghostty, sketchybar, nvim, btop

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -4,12 +4,12 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.keys = {
-  -- {
-  --   key = "raw:36",
-  --   mods = "SHIFT",
-  --   action = wezterm.action.SendString("\033[\015;2u"),
-  -- },
-  {key="Enter", mods="SHIFT", action=wezterm.action.SendString("\033[\015;2u")}
+	-- {
+	--   key = "raw:36",
+	--   mods = "SHIFT",
+	--   action = wezterm.action.SendString("\033[\015;2u"),
+	-- },
+	{ key = "Enter", mods = "SHIFT", action = wezterm.action.SendString("\033[\015;2u") },
 }
 
 -- config.font = wezterm.font("MesloLGS Nerd Font Mono")
@@ -20,7 +20,7 @@ config.initial_cols = 120
 config.max_fps = 120
 config.enable_kitty_graphics = true
 
-config.enable_tab_bar = false
+config.enable_tab_bar = true
 
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 1
@@ -28,6 +28,5 @@ config.macos_window_background_blur = 10
 config.native_macos_fullscreen_mode = true
 
 config.color_scheme = "Catppuccin Frappe"
-
 
 return config
